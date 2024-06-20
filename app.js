2e5afee688b0bd433b7de7073f4b7d8a9553c9c1
Brian McGrath <brian.joseph.mcgrath@gmail.com>
2024-06-20 14:20:13 +0100

    Added basic auth

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const emails = require('./routes/emails');
+const auth = require('./auth');
 
 const app = express();
 const port = 8080;
-const api = '/api';
 
 const addRoutes = (path, routes) => app.use(`/api/${path}`, routes);
+
+app.use(auth);
 
 app.get('/up', (req, res) => {
   res.send('Server is up!');
