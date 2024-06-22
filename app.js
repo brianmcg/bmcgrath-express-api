38e904ea7e43dcced93ab07aa54dae3db6e1ff4d
Brian McGrath <brian.joseph.mcgrath@gmail.com>
2024-06-22 13:19:40 +0100

    Added compression, helmet, express-rate-limit, express-minify

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const compression = require('compression');
+const helmet = require('helmet');
+const RateLimit = require('express-rate-limit');
+const minify = require('express-minify');
+
 const logger = require('./utils/logger');
 const auth = require('./utils/auth');
 const emails = require('./routes/emails');
@@ -10,8 +15,11 @@ const app = express();
 
 const addApiRoute = (path, routes) => app.use(`/api/${path}`, routes);
 
+app.use(compression());
+app.use(minify());
+app.use(helmet());
 app.use(cors());
-
+app.use(RateLimit({ windowMs: 60000, max: 20 }));
 app.use(auth.basic);
 
 app.get('/up', (req, res) => res.send('Server is up and running!'));
