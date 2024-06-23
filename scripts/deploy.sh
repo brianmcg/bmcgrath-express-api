05afd4989875f539272e1b52712c657771cbba7f
Brian McGrath <brian.joseph.mcgrath@gmail.com>
2024-06-23 18:11:01 +0100

    Updated deploy script and moved nginx config file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-function output() {
+REPO_PATH="/home/azureuser/apps/bmcgrath-express-api"
+NGINX_AVAILABLE_PATH="/home/azureuser/nginx/sites-available"
+NGINX_ENABLED_PATH="/home/azureuser/nginx/sites-enabled"
+NGINX_CONFIG_FILE="bmcgrath-express-api.conf"
+
+function echo_box() {
   string="| ${1} |"
   length=${#string}-2
   divider="+"
@@ -19,18 +24,21 @@ function output() {
 
 function deploy () {
   . ~/.nvm/nvm.sh
-  cd "/home/azureuser/apps/bmcgrath-express-api"
+  cd $REPO_PATH
 
-  output "Running git pull"
+  echo_box "Running git pull"
   git pull
 
-  output "Running npm install"
+  echo_box "Running npm install"
   npm install
 
-  output "Running npm build"
+  echo_box "Running npm build"
   npm run build
 
-  output "Reloading nginx"
+  echo_box "Reloading nginx"
+  sudo rm "${NGINX_ENABLED_PATH}/*"
+  sudo cp "config/${NGINX_CONFIG_FILE} ${NGINX_AVAILABLE_PATH}/"
+  sudo ln -s "${NGINX_AVAILABLE_PATH}/${NGINX_CONFIG_FILE}" "${NGINX_ENABLED_PATH}/${NGINX_CONFIG_FILE}"
   sudo service nginx reload
   sudo service nginx status
 }
