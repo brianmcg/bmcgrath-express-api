780b79356fd930ebba6477d75394baecb778f33c
Brian McGrath <brian.joseph.mcgrath@gmail.com>
2024-06-23 14:31:16 +0100

    Updated deploy scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
 
-# scp -r dist/* bm:~/www/
-scp -rp dist/* azure:~/www/bmcgrath-express-api
-scp -rp dist/.env azure:~/www/bmcgrath-express-api
+function output() {
+  string="| ${1} |"
+  length=${#string}-2
+  divider="+"
+
+  for ((i = 0; i < length; i++)); do
+    divider="${divider}-"
+  done
+
+  divider="${divider}+"
+
+  echo
+  echo ${divider}
+  echo ${string}
+  echo ${divider}
+  echo
+}
+
+function deploy () {
+  output "Starting deploy"
+  . ~/.nvm/nvm.sh
+  cd "/home/azureuser/apps/bmcgrath-express-api/"
+
+  output "Running npm install"
+  npm install
+
+  output "Running npm build"
+  npm run build
+
+  output "Restarting nginx"
+  sudo service nginx restart
+}
+
+ssh azure "$(typeset -f); deploy"
