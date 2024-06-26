3a51b3a937ce1cd4a0bf90009adce916b000ce6b
Brian McGrath <brian.joseph.mcgrath@gmail.com>
2024-06-26 21:00:28 +0100

    Fixed model name

--- src/models/post.js
+++ src/models/post.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
 const db = require('@config/database');
 
-const Post = db.define('Post', {
+const Post = db.define('post', {
   name: DataTypes.STRING,
   address: DataTypes.STRING,
   message: DataTypes.STRING,
