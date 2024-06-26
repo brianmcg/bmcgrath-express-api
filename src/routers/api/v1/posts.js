517fca500b0b65841535275ee9944addc0a92041
Brian McGrath <brian.joseph.mcgrath@gmail.com>
2024-06-27 00:35:23 +0100

    Moved controller logic to router

--- src/routers/api/v1/posts.js
+++ src/routers/api/v1/posts.js
@@ -1,18 +1,76 @@
 const express = require('express');
-const postsController = require('@controllers/posts');
-const bodyParser = require('body-parser')
-
+const bodyParser = require('body-parser');
+const { Post } = require('@models');
+const Logger = require('@utils/logger');
 const jsonParser = bodyParser.json()
 const router = express.Router();
 
-router.get('/', jsonParser, postsController.getAll);
+router.get('/', jsonParser, async (req, res) => {
+  try {
+    const posts = await Post.findAll();
+    res.json(posts);
+  } catch (error) {
+    Logger.error(error.toString())
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
-router.get('/:id', jsonParser, postsController.getById);
+router.get('/:id', jsonParser, async (req, res) => {
+  try {
+    const post = await Post.findByPk(req.params.id);
 
-router.post('/', jsonParser, postsController.create);
+    if (post) {
+      res.json(post);
+    } else {
+      res.status(404).json({ error: 'Post not found' });
+    }
+  } catch (error) {
+    Logger.error(error.toString())
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
-router.put('/:id',jsonParser, postsController.update);
+router.post('/', jsonParser, async (req, res) => {
+  try {
+    const post = await Post.create(req.body);
+    res.status(201).json(post);
+  } catch (error) {
+    Logger.error(error.toString())
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
-router.delete('/:id', jsonParser, postsController.delete);
+router.put('/:id',jsonParser, async (req, res) => {
+  try {
+    const post = await Post.findByPk(req.params.id);
+
+    if (post) {
+      post.set(req.body);
+      await post.save();
+      res.json(post);
+    } else {
+      res.status(404).json({ error: 'Post not found' });
+    }
+  } catch (error) {
+    Logger.error(error.toString())
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+router.delete('/:id', jsonParser, async (req, res) => {
+  try {
+    const post = await Post.findByPk(req.params.id);
+
+    if (post) {
+      await post.destroy();
+      res.json(post);
+    } else {
+      res.status(404).json({ error: 'Post not found' });
+    }
+  } catch (error) {
+    Logger.error(error.toString())
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 module.exports = router;
