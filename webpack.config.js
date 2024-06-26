50bbebf6a6b8d9d7191d8c60895888e985a074cf
Brian McGrath <brian.joseph.mcgrath@gmail.com>
2024-06-26 13:03:06 +0100

    Refactored webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,21 @@ const CopyPlugin = require('copy-webpack-plugin');
 const nodeExternals = require('webpack-node-externals');
 const { _moduleAliases: aliases } = require('./package.json');
 
+const resolve = p => path.resolve(__dirname, p);
+
 module.exports = env => ({
   mode: 'production',
   entry: './src/app.js',
   output: {
-    path: path.join(__dirname, env.path ? env.path : 'dist'),
+    path: resolve(env.path ? env.path : 'dist'),
     publicPath: '/',
     filename: 'app.min.js',
   },
   resolve: {
-    alias: Object.keys(aliases).reduce((memo, key) => ({
-      ...memo, [key]: path.resolve(__dirname, aliases[key]),
-    }), {}),
+    alias: Object.keys(aliases).reduce(
+      (memo, key) => ({ ...memo, [key]: resolve(aliases[key]) }),
+      {},
+    ),
   },
   externalsPresets: { node: true },
   externals: [nodeExternals()],
@@ -28,4 +31,3 @@ module.exports = env => ({
     }),
   ],
 });
-
